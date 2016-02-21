6c3804492a9553bb61aca2a5e196af114f00f41e
Tristan Graves <tristangraves4@gmail.com>
2016-02-20 23:23:27 -0600

    Started building up the structures for the MainMenuFragment

--- app/src/main/java/com/gravestristan/mypda/MainMenuFragment.java
+++ app/src/main/java/com/gravestristan/mypda/MainMenuFragment.java
@@ -1,12 +1,25 @@
 package com.gravestristan.mypda;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Tristan on 2/20/2016.
  */
 public class MainMenuFragment extends Fragment implements AppStatics{
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
