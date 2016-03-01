d321d9854841f43008d9d591dbae25128fab766b
Tristan Graves <tristangraves4@gmail.com>
2016-03-01 04:22:06 -0600

    Created basic fragment classes for each page

---
+++ app/src/main/java/com/gravestristan/mypda/WorkoutsMenuFragment.java
@@ -0,0 +1,23 @@
+package com.gravestristan.mypda;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tristan on 3/1/2016.
+ */
+public class WorkoutsMenuFragment extends Fragment implements AppStatics {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
